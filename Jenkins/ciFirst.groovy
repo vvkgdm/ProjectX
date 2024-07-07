3e923d31c7f9c5b1fb8960c36b9fb1fd50ddd44f
vivekananda <vivek.gdm@gmail.com>
2024-07-07 10:06:52 +0530

    Update ciFirst.groovy

--- Jenkins/ciFirst.groovy
+++ Jenkins/ciFirst.groovy
@@ -1,10 +1,5 @@
 pipeline {
-     agent {
-        docker {
-            image 'docker:dind'
-            args '--privileged'
-        }
-    }
+    agent any
 
     environment {
         GITHUB_REPO = 'vvkgdm/ProjectX'
@@ -18,29 +13,23 @@ pipeline {
         SCANNER_HOME = tool 'sonar-scanner'
     }
 
-        stages {
+    stages {
         stage('Checkout') {
             steps {
-                script {
-                    docker.image('alpine/git').inside {
-                        git url: "https://github.com/${GITHUB_REPO}", branch: "${BRANCH_NAME}", credentialsId: "${GIT_CREDS}"
-                    }
-                }
+                git url: "https://github.com/${GITHUB_REPO}", branch: "${BRANCH_NAME}", credentialsId: "${GIT_CREDS}"
             }
         }
 
         stage('Identify Changed Services') {
             steps {
                 script {
-                    docker.image('alpine').inside {
-                        def changedFiles = sh(script: 'git diff-tree --no-commit-id --name-only -r HEAD', returnStdout: true).trim().split('\n')
-                        def services = ['frontend', 'cartservice', 'productcatalogservice', 'currencyservice', 'paymentservice', 'shippingservice', 'emailservice', 'checkoutservice', 'recommendationservice', 'adservice', 'loadgenerator', 'shoppingassistantservice']
-                        env.CHANGED_SERVICES = services.findAll { service -> 
-                            changedFiles.any { it.contains(service) }
-                        }.join(',')
-                        if (env.CHANGED_SERVICES.isEmpty()) {
-                            error("No relevant services changed.")
-                        }
+                    def changedFiles = sh(script: 'git diff-tree --no-commit-id --name-only -r HEAD', returnStdout: true).trim().split('\n')
+                    def services = ['frontend', 'cartservice', 'productcatalogservice', 'currencyservice', 'paymentservice', 'shippingservice', 'emailservice', 'checkoutservice', 'recommendationservice', 'adservice', 'loadgenerator', 'shoppingassistantservice']
+                    env.CHANGED_SERVICES = services.findAll { service -> 
+                        changedFiles.any { it.contains(service) }
+                    }.join(',')
+                    if (env.CHANGED_SERVICES.isEmpty()) {
+                        error("No relevant services changed.")
                     }
                 }
             }
@@ -54,11 +43,8 @@ pipeline {
                 script {
                     def services = env.CHANGED_SERVICES.split(',')
                     services.each { service ->
-                        def dockerImage = getDockerImageForService(service)
-                        docker.image(dockerImage).inside {
-                            dir(service) {
-                                sh 'trivy fs --format table -o trivy-fs-report.html .'
-                            }
+                        dir(service) {
+                            sh 'trivy fs --format table -o trivy-fs-report.html .'
                         }
                     }
                 }
@@ -73,17 +59,14 @@ pipeline {
                 script {
                     def services = env.CHANGED_SERVICES.split(',')
                     services.each { service ->
-                        def dockerImage = getDockerImageForService(service)
-                        docker.image(dockerImage).inside {
-                            dir(service) {
-                                withSonarQubeEnv('sonar') {
-                                    sh """
-                                        ${SCANNER_HOME}/bin/sonar-scanner \
-                                        -Dsonar.host.url=${SONAR_URL} \
-                                        -Dsonar.projectKey=${service} \
-                                        -Dsonar.projectName=${service}
-                                    """
-                                }
+                        dir(service) {
+                            withSonarQubeEnv('sonar') {
+                                sh """
+                                    ${SCANNER_HOME}/bin/sonar-scanner \
+                                    -Dsonar.host.url=${SONAR_URL} \
+                                    -Dsonar.projectKey=${service} \
+                                    -Dsonar.projectName=${service}
+                                """
                             }
                         }
                     }
@@ -99,12 +82,10 @@ pipeline {
                 script {
                     def services = env.CHANGED_SERVICES.split(',')
                     services.each { service ->
-                        def dockerImage = getDockerImageForService(service)
-                        docker.image(dockerImage).inside {
-                            dir(service) {
-                                if (fileExists('Dockerfile')) {
-                                    docker.build("${NEXUS_REPO}/${service}:${DATE_TAG}").push()
-                                }
+                        dir(service) {
+                            if (fileExists('Dockerfile')) {
+                                sh "docker build -t ${NEXUS_REPO}/${service}:${DATE_TAG} ."
+                                sh "docker push ${NEXUS_REPO}/${service}:${DATE_TAG}"
                             }
                         }
                     }
@@ -120,10 +101,8 @@ pipeline {
                 script {
                     def services = env.CHANGED_SERVICES.split(',')
                     services.each { service ->
-                        docker.image('alpine').inside {
-                            sh "sysdig-inline-scan --docker-image ${NEXUS_REPO}/${service}:${DATE_TAG}"
-                            sh "trivy image ${NEXUS_REPO}/${service}:${DATE_TAG}"
-                        }
+                        sh "sysdig-inline-scan --docker-image ${NEXUS_REPO}/${service}:${DATE_TAG}"
+                        sh "trivy image ${NEXUS_REPO}/${service}:${DATE_TAG}"
                     }
                 }
             }
@@ -138,16 +117,14 @@ pipeline {
                     script {
                         def services = env.CHANGED_SERVICES.split(',')
                         services.each { service ->
-                            docker.image('alpine/git').inside {
-                                dir(service) {
-                                    sh """
-                                        git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/${GITHUB_REPO}.git
-                                        cd ${GITHUB_REPO}
-                                        sed -i 's/tag:.*/tag: ${DATE_TAG}/g' values-${BRANCH_NAME}.yaml
-                                        git commit -am 'Update image tag to ${DATE_TAG}'
-                                        git push
-                                    """
-                                }
+                            dir(service) {
+                                sh """
+                                    git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/${GITHUB_REPO}.git
+                                    cd ${GITHUB_REPO}
+                                    sed -i 's/tag:.*/tag: ${DATE_TAG}/g' values-${BRANCH_NAME}.yaml
+                                    git commit -am 'Update image tag to ${DATE_TAG}'
+                                    git push
+                                """
                             }
                         }
                     }
@@ -158,33 +135,7 @@ pipeline {
 
     post {
         always {
-            script {
-                docker.image('alpine').inside {
-                    cleanWs()
-                }
-            }
+            cleanWs()
         }
-    }
-}
-def getDockerImageForService(service) {
-    switch (service) {
-        case 'frontend':
-        case 'productcatalogservice':
-        case 'shippingservice':
-        case 'checkoutservice':
-            return 'golang:1.16'
-        case 'cartservice':
-            return 'mcr.microsoft.com/dotnet/sdk:5.0'
-        case 'currencyservice':
-        case 'paymentservice':
-            return 'node:14'
-        case 'emailservice':
-        case 'recommendationservice':
-        case 'loadgenerator':
-            return 'python:3.8'
-        case 'adservice':
-            return 'openjdk:11'
-        default:
-            return 'alpine'
     }
 }
