3cc7bc63b87c797b7b12ffbd8d1dbb580d171fa6
vivekananda <vivek.gdm@gmail.com>
2024-07-07 19:27:03 +0530

    Update ciFirst.groovy

--- Jenkins/ciFirst.groovy
+++ Jenkins/ciFirst.groovy
@@ -124,7 +124,7 @@ pipeline {
                                 sh """
                                     git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/${GITHUB_HELM_REPO}.git
                                     cd ${GITHUB_HELM_REPO}
-                                    sed -i 's/tag:.*/tag: ${DATE_TAG}/g' values-${HELM_BRANCH_NAME}.yaml
+                                    sed -i 's/tag:.*/tag: ${DATE_TAG}/g' values-${BRANCH_NAME}.yaml
                                     git commit -am 'Update image tag to ${DATE_TAG}'
                                     git push
                                 """
