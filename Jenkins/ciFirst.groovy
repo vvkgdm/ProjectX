841668b57035ff79e817412f53b37689615a6844
vivekananda <vivek.gdm@gmail.com>
2024-07-07 21:22:46 +0530

    Update ciFirst.groovy

--- Jenkins/ciFirst.groovy
+++ Jenkins/ciFirst.groovy
@@ -134,7 +134,7 @@ pipeline {
                             dir("ProjectXHelm/${service}") {
                                 sh """
                                     git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/vvkgdm/${GITHUB_HELM_REPO}.git
-                                    cd ${GITHUB_HELM_REPO}
+                                    cd ${GITHUB_HELM_REPO}/${service}
                                     pwd
                                     ls
                                     sed -i 's/tag:.*/tag: ${DATE_TAG}/g' values-${BRANCH_NAME}.yaml
