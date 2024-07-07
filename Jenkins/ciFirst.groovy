02c3ccabff28d555f1de9ae290e4ca3daa35943f
vivekananda <vivek.gdm@gmail.com>
2024-07-07 10:21:54 +0530

    Update ciFirst.groovy

--- Jenkins/ciFirst.groovy
+++ Jenkins/ciFirst.groovy
@@ -3,12 +3,11 @@ pipeline {
 
     environment {
         GITHUB_REPO = 'vvkgdm/ProjectX'
-        BRANCH_NAME = 'development'
+        BRANCH_NAME = 'dit'
         NEXUS_REPO = 'http://54.92.241.126:8081/repository/docker-repo'
         NEXUS_URL = 'http://54.92.241.126:8081/'
         SONAR_URL = 'http://54.92.241.126:9000/'
         DATE_TAG = "${new Date().format('yyyyMMddHHmmss')}"
-        //DOCKER_CREDS = credentials('docker-credentials-id')
         GIT_CREDS = credentials('githubID')
         SCANNER_HOME = tool 'sonar-scanner'
     }
@@ -17,19 +16,24 @@ pipeline {
         stage('Checkout') {
             steps {
                 git url: "https://github.com/${GITHUB_REPO}", branch: "${BRANCH_NAME}", credentialsId: "${GIT_CREDS}"
+                script {
+                    env.WORKSPACE = "${WORKSPACE}/SourceCode"
+                }
             }
         }
 
         stage('Identify Changed Services') {
             steps {
-                script {
-                    def changedFiles = sh(script: 'git diff-tree --no-commit-id --name-only -r HEAD', returnStdout: true).trim().split('\n')
-                    def services = ['frontend', 'cartservice', 'productcatalogservice', 'currencyservice', 'paymentservice', 'shippingservice', 'emailservice', 'checkoutservice', 'recommendationservice', 'adservice', 'loadgenerator', 'shoppingassistantservice']
-                    env.CHANGED_SERVICES = services.findAll { service -> 
-                        changedFiles.any { it.contains(service) }
-                    }.join(',')
-                    if (env.CHANGED_SERVICES.isEmpty()) {
-                        error("No relevant services changed.")
+                dir(env.WORKSPACE) {
+                    script {
+                        def changedFiles = sh(script: 'git diff-tree --no-commit-id --name-only -r HEAD', returnStdout: true).trim().split('\n')
+                        def services = ['frontend', 'cartservice', 'productcatalogservice', 'currencyservice', 'paymentservice', 'shippingservice', 'emailservice', 'checkoutservice', 'recommendationservice', 'adservice', 'loadgenerator', 'shoppingassistantservice']
+                        env.CHANGED_SERVICES = services.findAll { service -> 
+                            changedFiles.any { it.contains("SourceCode/${service}") }
+                        }.join(',')
+                        if (env.CHANGED_SERVICES.isEmpty()) {
+                            error("No relevant services changed.")
+                        }
                     }
                 }
             }
@@ -43,7 +47,7 @@ pipeline {
                 script {
                     def services = env.CHANGED_SERVICES.split(',')
                     services.each { service ->
-                        dir(service) {
+                        dir("${env.WORKSPACE}/${service}") {
                             sh 'trivy fs --format table -o trivy-fs-report.html .'
                         }
                     }
@@ -59,7 +63,7 @@ pipeline {
                 script {
                     def services = env.CHANGED_SERVICES.split(',')
                     services.each { service ->
-                        dir(service) {
+                        dir("${env.WORKSPACE}/${service}") {
                             withSonarQubeEnv('sonar') {
                                 sh """
                                     ${SCANNER_HOME}/bin/sonar-scanner \
@@ -82,7 +86,7 @@ pipeline {
                 script {
                     def services = env.CHANGED_SERVICES.split(',')
                     services.each { service ->
-                        dir(service) {
+                        dir("${env.WORKSPACE}/${service}") {
                             if (fileExists('Dockerfile')) {
                                 sh "docker build -t ${NEXUS_REPO}/${service}:${DATE_TAG} ."
                                 sh "docker push ${NEXUS_REPO}/${service}:${DATE_TAG}"
@@ -117,11 +121,13 @@ pipeline {
                     script {
                         def services = env.CHANGED_SERVICES.split(',')
                         services.each { service ->
-                            dir(service) {
+                            dir("${env.WORKSPACE}/${service}") {
                                 sh """
                                     git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/${GITHUB_REPO}.git
                                     cd ${GITHUB_REPO}
                                     sed -i 's/tag:.*/tag: ${DATE_TAG}/g' values-${BRANCH_NAME}.yaml
+                                    git config user.email "jenkins@example.com"
+                                    git config user.name "Jenkins"
                                     git commit -am 'Update image tag to ${DATE_TAG}'
                                     git push
                                 """
